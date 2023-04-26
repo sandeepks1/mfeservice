58e7248d828955bba0f213b96336fe7d354865ad
sandeepks1 <sandeep.ks1@gmail.com>
2023-04-26 17:08:18 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,23 @@ const app = express();
 // Define the API endpoint
 app.get('/api/manifest', (req, res) => {
     // Define the JSON response
-    const response = [{ html: '<script src="https://mfeservices.onrender.com/runtime.89424a2453fbce0c.js" type="module"></script> <script src="https://mfeservices.onrender.com/polyfills.5f77ae0f37392226.js" type="module"></script> <script src="https://mfeservices.onrender.com/main.737a91333ae6f91c.js" type="module"></script>', selector: 'head' }];
+    const response = [{
+            selector: "head",
+            html: "<script src='https://mfeservices.onrender.com/runtime.89424a2453fbce0c.js' type='module' defer></script>"
+        },
+        {
+            selector: "head",
+            html: "<script src='https://mfeservices.onrender.com/polyfills.5f77ae0f37392226.js' type='module'  defer></script>"
+        },
+        {
+            selector: "head",
+            html: "<script src='https://mfeservices.onrender.com/main.737a91333ae6f91c.js' type='module'  defer></script>"
+        },
+        {
+            selector: "head",
+            html: "<link rel='stylesheet' href='https://mfeservices.onrender.com/styles.f61d972cab9048d4.css' />"
+        }
+    ];
 
     // Send the JSON response
     res.json(response);
@@ -14,22 +30,3 @@ app.get('/api/manifest', (req, res) => {
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
-
-
-[{
-        selector: "head",
-        html: "<script src='https://mfeservices.onrender.com/runtime.89424a2453fbce0c.js' type='module' defer></script>"
-    },
-    {
-        selector: "head",
-        html: "<script src='https://mfeservices.onrender.com/polyfills.5f77ae0f37392226.js' type='module'  defer></script>"
-    },
-    {
-        selector: "head",
-        html: "<script src='https://mfeservices.onrender.com/main.737a91333ae6f91c.js' type='module'  defer></script>"
-    },
-    {
-        selector: "head",
-        html: "<link rel='stylesheet' href='https://mfeservices.onrender.com/styles.f61d972cab9048d4.css' />"
-    }
-];
